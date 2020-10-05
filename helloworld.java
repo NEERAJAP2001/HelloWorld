e0bad766a2004fda030ae0c9bd7ae7171a942344
rajdeeptechky <55846486+RajdeepDey010@users.noreply.github.com>
2020-10-05 17:56:36 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+//From Rajdeep
+import java.io.*;
+class Hello
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+    }
+  }
+}
+
+// thankyou
+// Dropped a Star to your repo.
